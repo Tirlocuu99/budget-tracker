519d1059708831aaf9014821b665af3936479b38
Tirlocuu99 <tirlaandrei99@gmail.com>
2026-05-06 13:17:26 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,10 @@
-const CACHE_NAME = "budget-flow-v1";
-const urlsToCache = [
-  "./",
-  "./index.html"
-];
+const CACHE_NAME = "tirlocuu-budget-flow-v1";
+const urlsToCache = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
